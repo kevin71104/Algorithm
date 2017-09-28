19963a658154a411b79d839b5fdc012b4fa34eb1
kevin71104 <b03901026@ntu.edu.tw>
2017-09-28 19:42:38 +0800

    P2 pass testbench

--- HW1/P2/match.py
+++ HW1/P2/match.py
@@ -2,18 +2,35 @@ import sys
 ########################################
 # You can add supporting functions here
 
-
-
-
-
 ########################################
 def match(men, women, mprefer, wprefer):
     ### TODO ###
     # Implement the Gale-Shapley algorithm.
-    # You should return a dictionary of pairing result, use woman as key and man as
-    # value.
+    # You should return a dictionary of pairing result, use woman as key and
+    # man as value.
+    # The returned dictionary 'wives' is composed of key 'woman' to a value 'man'
 
+    num = len(men[0])
+    menlist = [i for i in range(num)]
+    matchrecord = [[False for j in range(num)] for i in range(num)]
     wives = {}
+
+    while len(menlist) > 0 :
+        manIdx = menlist[0]
+
+        unmatchIdx = matchrecord[manIdx].index(False) # priorest unmatched woman
+        womanIdx = mprefer[manIdx][unmatchIdx]
+        matchrecord[manIdx][unmatchIdx] = True
+
+        if womanIdx in wives:
+            originIdx = wives[womanIdx]
+            if women[womanIdx][originIdx] > women[womanIdx][manIdx]:
+                wives[womanIdx] = manIdx
+                del menlist[0]
+        else :
+            wives.update({womanIdx : manIdx})
+            del menlist[0]
+
     return wives
 
 
@@ -25,7 +42,7 @@ def is_stable(men, women, mprefer, wives):
         preferred = mprefer[m][:i]
         for p in preferred:
             h = wives[p]
-            if women[p][m] < women[p][h]:  
+            if women[p][m] < women[p][h]:
                 return False
     return True
 
@@ -33,15 +50,15 @@ if __name__ == '__main__':
     '''
     A reminder of the definitions of variables
 
-    men is a 2D-list which presents men's rankings for women. 
+    men is a 2D-list which presents men's rankings for women.
     men[m][w]=i means man 'm' ranks woman 'w' as i.
     Note that every list is zero-based and 0 means the highest preference.
-    For example, if there are 10 pairs of men and women, 
+    For example, if there are 10 pairs of men and women,
     they are denoted as m0, m1, ..., m9 and w0, w1, ..., w9
     men[1][2] = 0 means m1 likes w2 the most among all women.
     Similarly, women[1][2] = 1 means, for w1, m2 is her second choice.
-    
-    mprefer is a 2D-list which presents men's preference.  
+
+    mprefer is a 2D-list which presents men's preference.
     mprefer[m][i]=w means, for man 'm', his i-th preferable woman is 'w'
     For examples, mprefer[1][0]=2 means m1's first choice is w2.
     '''
@@ -53,6 +70,7 @@ if __name__ == '__main__':
         man = list(map(int, line.split()))
         men.append(man)
 
+
     women = []
     for line in open(women_file, 'r'):
         woman = list(map(int, line.split()))
@@ -60,11 +78,11 @@ if __name__ == '__main__':
 
     wives = {}
     pairs = []
-    
+
     mprefer = []
     wprefer = []
     pair_num = len(men[0])
-    
+
     for i in range(pair_num):
         mp = []
         wp = []
@@ -73,16 +91,8 @@ if __name__ == '__main__':
             wp.append(women[i][:].index(j))
         mprefer.append(mp)
         wprefer.append(wp)
-    
     wives = match(men, women, mprefer, wprefer)
     if is_stable(men, women, mprefer, wives):
         print('The matching is stable.')
     else:
         print('The matching is failed.')
-
-
-
-
-
-
-
